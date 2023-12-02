de6432a7bdf96f817a0eb96af00847ab7c90df0d
KarolPabian <karolpabian0@gmail.com>
2023-12-02 15:11:13 +0000

    Add listAllDoctors()

--- src/main/kotlin/controllers/DoctorAPI.kt
+++ src/main/kotlin/controllers/DoctorAPI.kt
@@ -9,4 +9,17 @@ class DoctorAPI {
     fun add(doctor: Doctor): Boolean {
         return doctors.add(doctor)
     }
+
+    fun listAllDoctors(): String {
+        return if (doctors.isEmpty()) {
+            "No doctors stored"
+        } else {
+            var listOfDoctors = ""
+            for (i in doctors.indices) {
+                listOfDoctors += "${i + 1}: ${doctors[i]} \n"
+            }
+            listOfDoctors
+
+        }
+    }
 }
--- src/main/kotlin/controllers/PatientAPI.kt
+++ src/main/kotlin/controllers/PatientAPI.kt
@@ -9,4 +9,15 @@ class PatientAPI {
     fun add(patient: Patient): Boolean {
         return patients.add(patient)
     }
+    fun listAllPatients(): String {
+        return if (patients.isEmpty()) {
+            "No patients stored"
+        } else {
+            var listOfPatients = ""
+            for (i in patients.indices) {
+                listOfPatients += "${i + 1}: ${patients[i]} \n"
+            }
+            listOfPatients
+        }
+    }
 }
