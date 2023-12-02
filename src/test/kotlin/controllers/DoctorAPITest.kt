a7071ff85a15d25735de035b272129ad656e8737
KarolPabian <karolpabian0@gmail.com>
2023-12-02 18:49:46 +0000

    Implement JUnit testing for adding a doctor

--- src/test/kotlin/controllers/DoctorAPITest.kt
+++ src/test/kotlin/controllers/DoctorAPITest.kt
@@ -1,4 +1,36 @@
-package controllers
+import controllers.DoctorAPI
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import models.Doctor
 
 class DoctorAPITest {
+
+    @Test
+    fun `adding a doctor to a populated list adds to ArrayList`() {
+
+        val doctorAPI = DoctorAPI()
+        val newDoctor = Doctor(1, "Dr. Smith", "General Medicine", "123-456-7890")
+
+
+        assertTrue(doctorAPI.add(newDoctor))
+
+
+        assertEquals(1, doctorAPI.numberOfDoctors())
+        assertEquals(newDoctor, doctorAPI.findDoctor(0))
+    }
+
+    @Test
+    fun `adding a doctor to an empty list adds to ArrayList`() {
+
+        val doctorAPI = DoctorAPI()
+        val newDoctor = Doctor(1, "Dr. Smith", "General Medicine", "123-456-7890")
+
+
+        assertTrue(doctorAPI.add(newDoctor))
+
+
+        assertEquals(1, doctorAPI.numberOfDoctors())
+        assertEquals(newDoctor, doctorAPI.findDoctor(0))
+    }
 }
