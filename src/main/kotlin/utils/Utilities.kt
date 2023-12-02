2a64e1b484d921b1ca0aa894aa3861a87a72dd0f
KarolPabian <karolpabian0@gmail.com>
2023-12-02 15:21:11 +0000

    Add validRange function

--- src/main/kotlin/utils/Utilities.kt
+++ src/main/kotlin/utils/Utilities.kt
@@ -8,4 +8,8 @@ object Utilities {
     fun isValidListIndex(index: Int, list: List<Any>): Boolean {
         return (index >= 0 && index < list.size)
     }
+    @JvmStatic
+    fun validRange(numberToCheck: Int, min: Int, max: Int): Boolean {
+        return numberToCheck in min..max
+    }
 }
