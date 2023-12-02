475f271165a62f4dd516925905be9ebbc80adfd2
KarolPabian <karolpabian0@gmail.com>
2023-12-02 18:51:56 +0000

    Implement JUnit testing for listing doctors

--- src/test/kotlin/controllers/DoctorAPITest.kt
+++ src/test/kotlin/controllers/DoctorAPITest.kt
@@ -33,4 +33,29 @@ class DoctorAPITest {
         assertEquals(1, doctorAPI.numberOfDoctors())
         assertEquals(newDoctor, doctorAPI.findDoctor(0))
     }
+    @Test
+    fun `listAllDoctors returns No Doctors Stored message when ArrayList is empty`() {
+
+        val emptyDoctorAPI = DoctorAPI()
+
+
+        assertEquals(0, emptyDoctorAPI.numberOfDoctors())
+        assertTrue(emptyDoctorAPI.listAllDoctors().lowercase().contains("no doctors"))
+    }
+
+    @Test
+    fun `listAllDoctors returns Doctors when ArrayList has doctors stored`() {
+
+        val doctorAPI = DoctorAPI()
+        val newDoctor = Doctor(1, "Dr. Smith", "General Medicine", "123-456-7890")
+        doctorAPI.add(newDoctor)
+
+
+        val doctorsString = doctorAPI.listAllDoctors().lowercase()
+
+
+        assertEquals(1, doctorAPI.numberOfDoctors())
+        assertTrue(doctorsString.contains("dr. smith"))
+    }
 }
+
